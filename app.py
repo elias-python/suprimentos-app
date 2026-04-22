efc9b148f2692172ee6e009e4041b307183f3ef9
elias-python <andreelias704@gmail.com>
2026-04-22 09:37:49 -0300

    supabase1

--- app.py
+++ app.py
@@ -838,4 +838,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5050, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host='0.0.0.0', port=port)
